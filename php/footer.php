f681b30919fea998f30296028d0820120651e1cc
Pedro Sicilia <pedrosm1991@gmail.com>
2017-06-14 14:51:11 +0200

    Pequeño cambio en el diseño del footer

--- php/footer.php
+++ php/footer.php
@@ -2,41 +2,55 @@
                 <div class="footer" id="footer">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 mg-tp-40 text-left">
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 mg-tp-40 text-lcenter">
                             <img class="img-responsive" src="images/logo-merideando.png">
+                            <ul class="social">
+                                <li> <a href="#"> <i class=" fa fa-facebook"> </i></a></li>
+                                <li> <a href="#"> <i class="fa fa-twitter"> </i></a></li>
+                                <li> <a href="#"> <i class="fa fa-instagram"> </i></a></li>
+                            </ul>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-3 hidden-xs">
                             <h3> Merideando </h3>
                             <ul>
                                 <li> <a href="index.php"> Inicio </a> </li>
                                 <li> <a href="nosotros.php"> Nosotros</a></li>
-                                <li> <a href="categorias.php"> Categorías </a> </li>
-                                <li> <a href="panel_usuario.php"> Tu cuenta </a> </li>
+                                <li> <a href="registro.php"> Regístrate </a> </li>
+                                <li> <a href="index.php"> Contacto </a></li>
+                                
                                 
                             </ul>
                         </div>
                         <div class="col-lg-3  col-md-3 col-sm-3 hidden-xs">
                             <h3> Anuncios </h3>
                             <ul>
+                                 <li> <a href="#"> Buscador </a></li>
                                 <li> <a href="categorias.php"> Categorías </a></li>
                                 <li> <a href="mapa_anuncios.php"> Mapa de anuncios </a></li>
                                 <li> <a href="panel_usuario.php"> Panel Administración </a></li>
-                                <li> <a href="index.php"> Contacto </a></li>
+                                
                             </ul>
                         </div>
                         
                         <div class="col-lg-3  col-md-3 col-sm-3 col-xs-6 pull-right ">
                             <h3>Contacto </h3>
                             <ul>
-                                <li><i class="fa fa-map-marker" aria-hidden="true"></i> Calle John Lennon, 36. 06800 Mérida </li>
-                                <li><i class="fa fa-phone" aria-hidden="true"></i> Llámanos al +34 645590102</li>
-                                <li><i class="fa fa-envelope-o" aria-hidden="true"></i> pedrosm1991@gmail.com</li> 
+                                <li><span class="fa-stack fa-lg">
+                                      <i class="fa fa-circle fa-stack-2x"></i>
+                                      <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                                    </span> C/ John Lennon, 36 
+                                </li>
+                                <li><span class="fa-stack fa-lg">
+                                      <i class="fa fa-circle fa-stack-2x"></i>
+                                      <i class="fa fa-phone fa-stack-1x fa-inverse"></i>
+                                    </span> +34 645590102</li>
+                               
+                                <li><span class="fa-stack fa-lg">
+                                      <i class="fa fa-circle fa-stack-2x"></i>
+                                      <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                    </span> github/pedrosici</li>
                             </ul>
-                            <ul class="social">
-                                <li> <a href="#"> <i class=" fa fa-facebook"> </i></a></li>
-                                <li> <a href="#"> <i class="fa fa-twitter"> </i></a></li>
-                                <li> <a href="#"> <i class="fa fa-instagram"> </i></a></li>
-                            </ul>
+                            
                         </div>
                     </div>
                     <!--/.row--> 
